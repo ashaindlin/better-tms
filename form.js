e92ba8241a8fbb00b2c0ae6599c38c59890059a9
Alex Shaindlin <ashaindlin@gmail.com>
2014-10-17 12:19:28 -0400

    Move form creation to separate module

---
+++ form.js
@@ -0,0 +1,16 @@
+module.exports.url = 'https://duapp2.drexel.edu/webtms_du/app';
+
+module.exports.buildForm = function(term, courseName, courseNumber) {
+    return {
+        formids: "term,courseName,crseNumb,crn",
+            component: "searchForm",
+            page: "Home",
+            service: "direct",
+            submitmode: "submit",
+            submitname: "",
+            term: term,
+            courseName: courseName,
+            crseNumb: courseNumber,
+            crn: ""
+    }
+}
