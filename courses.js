0b5d6763aa13c41596acc5535c3ec61ce729ec93
Trevor Heins <trevor.heins@stvinc.com>
2015-07-14 15:20:11 -0400

    Search by CRN now works
    Fixed search by CRN, before it was not working at all, now it works!

--- courses.js
+++ courses.js
@@ -18,38 +18,73 @@ module.exports.getCourses = function(req, res) {
             $ = cheerio.load(body);
             var results = {};
             var i = 0;
-            console.log("New request");
+            //console.log("New request");
             $(".even, .odd").each(function() {
 	            var obj = {};
                 var j = 0;
-                $(this).find('td [valign=top]').each(function() {
-                    switch (j) {
-                        case 0:
-                            obj['subjectcode'] = $(this).text();
-                            break;
-                        case 1:
-                            obj['coursenumber'] = $(this).text();
-                            break;
-                        case 2:
-                            obj['instrtype'] = $(this).text();
-                            break;
-                        case 3:
-                            obj['section'] = $(this).text();
-                            break;
-                        case 4:
-                            obj['crn'] = $(this).text();
-                            break;
-                        case 5:
-                            obj['coursetitle'] = $(this).text();
-                            break;
-                        case 6:
-                            obj['instructor'] = $(this).text().replace("  ", " ");
-                            break;
-                        default:
-                            break;
-                    }
-                    j++;
-                })
+                if (crn == '')
+                {
+	                $(this).find('td [valign=top]').each(function() {
+	                    switch (j) {
+	                        case 0:
+	                            obj['subjectcode'] = $(this).text();
+	                            break;
+	                        case 1:
+	                            obj['coursenumber'] = $(this).text();
+	                            break;
+	                        case 2:
+	                            obj['instrtype'] = $(this).text();
+	                            break;
+	                        case 3:
+	                            obj['section'] = $(this).text();
+	                            break;
+	                        case 4:
+	                            obj['crn'] = $(this).text();
+	                            break;
+	                        case 5:
+	                            obj['coursetitle'] = $(this).text();
+	                            break;
+	                        case 6:
+	                            obj['instructor'] = $(this).text().replace("  ", " ");
+	                            break;
+	                        default:
+	                            break;
+	                    }
+	                    j++;
+                	})
+                }
+                else
+                {
+	                $(this).find('td [valign="center"]').each(function() {
+	                    switch (j) {
+	                        case 0:
+	                            obj['subjectcode'] = $(this).text();
+	                            break;
+	                        case 1:
+	                            obj['coursenumber'] = $(this).text();
+	                            break;
+	                        case 2:
+	                            obj['instrtype'] = $(this).text();
+	                            break;
+	                        case 3:
+	                            obj['section'] = $(this).text();
+	                            break;
+	                        case 4:
+	                            obj['crn'] = $(this).text();
+	                            break;
+	                        case 5:
+	                            obj['coursetitle'] = $(this).text();
+	                            break;
+	                        case 6:
+	                            obj['instructor'] = $(this).text().replace("  ", " ");
+	                            break;
+	                        default:
+	                            break;
+	                    }
+	                    j++;
+                	})
+                }
+                
                 if("subjectcode" in obj)
                 {
 	                var k = 0;
